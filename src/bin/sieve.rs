6a286b242bf1df3bf28378888e38c3492b0aa3dd
Hidemoto Nakada <hide-nakada@aist.go.jp>
2019-02-14 12:32:53 +0900

    sieve

--- src/bin/sieve.rs
+++ src/bin/sieve.rs
@@ -2,10 +2,25 @@ fn main() {
     let mut flags = [true; 100];
     flags[0] = false;
     flags[1] = false;
+    let mut current = 2;
 
-    for i in 0..100 {
-        if i % 2 == 0 {
-            flags[i] = false
+    while current + 1< 100 {
+        for i in current + 1.. 100 {
+            if i % current == 0 {
+                flags[i] = false;
+            }
+        }
+        // find next prime
+        let mut found = false;
+        for i in current + 1 .. 100 {
+            if flags[i] == true {
+                current = i;
+                found = true;
+                break;
+            }
+        }
+        if ! found {
+            break;
         }
     }
 
